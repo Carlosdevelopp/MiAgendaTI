2382f515461f77d66daa0fe5c3ff135b79e1096f
Carlosdevelopp <carlosdevelopp@gmail.com>
2025-08-22 23:57:06 -0600

    feature: Add initial models for DataAccess layer

--- MiAgendaTI/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ MiAgendaTI/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57a050c63ed976570113f0d07e20a6fd82408c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
